3c14fe6fab35021a8ffba0d95020d11488cd2146
TPoe25 <t_poe25@yahoo.com>
2023-12-20 11:57:39 -0600

    fixed js and css to make hover and links working!!!

--- public/js/newscript2.js
+++ public/js/newscript2.js
@@ -1,62 +1,16 @@
-
 document.addEventListener('DOMContentLoaded', function () {
-  window.onload = setInterval(GameLoop, 1000 / 10);
+  const cards = document.querySelectorAll('.card');
 
-  document.addEventListener('click', function (event) {
-    const card = event.target.closest('.card');
-    if (card) {
-      card.closest('.cardContainer').classList.toggle('inactive');
-    }
+  function flipCard(event) {
+    const card = event.currentTarget;
+    card.classList.toggle('active');
+    card.closest('.cardContainer').classList.toggle('inactive');
+  }
+
+  // Add mouseover event listener to each card
+  cards.forEach(card => {
+    card.addEventListener('mouseover', flipCard);
   });
 
   console.log('Page loaded.');
 });
-
-const cards = document.querySelectorAll('.card');
-
-function transition() {
-  this.classList.toggle('active');
-  // Add the following line to ensure that the 'inactive' class is toggled as well
-  this.closest('.cardContainer').classList.toggle('inactive');
-}
-
-cards.forEach(card => card.addEventListener('click', transition));
-
-function handleStarRating() {
-  var ratingContainers = document.querySelectorAll('.ratingContainer');
-
-  ratingContainers.forEach(function (ratingContainer) {
-      // Create a function to handle the click event on a star
-      function handleStarClick(starNumber) {
-          return function () {
-              // Update the rating based on the clicked star
-              updateRating(ratingContainer, starNumber);
-          };
-      }
-
-      // Create a function to update the rating
-      function updateRating(container, selectedStars) {
-          // Remove existing stars
-          container.innerHTML = '';
-
-          // Add selected stars
-          for (var i = 1; i <= 5; i++) {
-              var star = document.createElement('i');
-              star.classList.add('fas', i <= selectedStars ? 'fa-star' : 'fa-star-o');
-              star.addEventListener('click', handleStarClick(i));
-              container.appendChild(star);
-          }
-
-          // Display the selected rating
-          var ratingText = document.createElement('p');
-          ratingText.textContent = 'Rating: ' + selectedStars + ' out of 5 stars';
-          container.appendChild(ratingText);
-      }
-
-      // Initialize the rating with 0 stars
-      updateRating(ratingContainer, 0);
-  });
-}
-
-// Call the function to initialize the star rating system
-handleStarRating();
